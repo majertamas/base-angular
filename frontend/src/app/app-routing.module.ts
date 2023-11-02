20688c35ac2906472c059fa408af5720da5e271b
majer.tamas <majer.tamas@mikrum.hu>
2023-11-02 10:07:41 +0100

    alap

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MDropdownComponent } from './component/m-dropdown/m-dropdown.component';
 
 const routes: Routes = [
-  { path: 'dropdown', component: MDropdownComponent }
+  
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,13 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputTextModule } from 'primeng/inputtext';
 import { DropdownModule } from 'primeng/dropdown';
-import { MDropdownComponent } from './component/m-dropdown/m-dropdown.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MDropdownComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
